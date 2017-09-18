2438341608f1c0fe751dbb76ae960620edf20138
Juan José Casafranca <jjcasmar@gmail.com>
2017-09-18 23:12:09 +0200

    Trying to share a scene through different engines

---
+++ src/apps/3DWindow/hwindow.h
@@ -0,0 +1,62 @@
+#ifndef HWINDOW_H
+#define HWINDOW_H
+
+#include <Qt3DCore>
+#include <Qt3DRender>
+#include <Qt3DLogic>
+#include <Qt3DExtras>
+
+class HWindow : public QWindow {
+    Q_OBJECT
+
+public:
+    HWindow (QWindow *parent = nullptr) :
+        m_engine(new Qt3DCore::QAspectEngine()),
+        m_renderAspect(new Qt3DRender::QRenderAspect()),
+        m_framegraph(new Qt3DExtras::QForwardRenderer),
+        m_rootEntity(new Qt3DCore::QEntity()),
+        m_renderSettings(new Qt3DRender::QRenderSettings()),
+        m_logicAspect(new Qt3DLogic::QLogicAspect()),
+        m_frameAction(new Qt3DLogic::QFrameAction())
+    {
+        setSurfaceType(QWindow::OpenGLSurface);
+
+        m_framegraph->setSurface(this);
+        m_engine->registerAspect(m_renderAspect);
+        m_engine->registerAspect(m_logicAspect);
+        m_renderSettings->setActiveFrameGraph(m_framegraph);
+        m_rootEntity->addComponent(m_renderSettings);
+        m_frameAction->setParent(m_framegraph);
+        m_engine->setRootEntity((Qt3DCore::QEntityPtr)m_rootEntity);
+    }
+
+
+    void setScene(Qt3DCore::QEntity *scene) {m_scene = scene;}
+    Qt3DCore::QEntity *rootEntity() {return m_rootEntity;}
+    Qt3DLogic::QFrameAction *frameAction() {return m_frameAction;}
+
+public Q_SLOTS:
+    void shutdown() {
+        m_scene->setParent((Qt3DCore::QNode*)nullptr);
+        m_renderSettings->setActiveFrameGraph(new Qt3DRender::QNoDraw);
+//        m_engine->setRootEntity((Qt3DCore::QEntityPtr)nullptr);
+    }
+
+    void turnOn() {
+        m_scene->setParent(m_rootEntity);
+        m_renderSettings->setActiveFrameGraph(m_framegraph);
+    }
+
+public:
+    Qt3DCore::QAspectEngine *m_engine;
+    Qt3DRender::QRenderAspect *m_renderAspect;
+    Qt3DExtras::QForwardRenderer *m_framegraph;
+    Qt3DCore::QEntity *m_rootEntity;
+    Qt3DLogic::QLogicAspect *m_logicAspect;
+    Qt3DLogic::QFrameAction *m_frameAction;
+
+    Qt3DCore::QEntity *m_scene;
+    Qt3DRender::QRenderSettings *m_renderSettings;
+};
+
+#endif // HWINDOW_H
--- src/apps/3DWindow/main.cpp
+++ src/apps/3DWindow/main.cpp
@@ -1,69 +1,38 @@
-#include "../../core/mainwindow.h"
-
 #include <QApplication>
 
 #include <Qt3DExtras>
 #include <Qt3DRender>
+#include <QDebug>
 
-#include "../../core/wireframematerial.h"
-#include "../../core/trackballcameracontroller.h"
+#include "hwindow.h"
 
 int main(int argc, char **argv) {
 
     QApplication app(argc, argv);
 
-    QSurfaceFormat format;
-    format.setProfile(QSurfaceFormat::CoreProfile);
-    format.setMajorVersion(3);
-    format.setMinorVersion(1);
-    QSurfaceFormat::setDefaultFormat(format);
-    MainWindow window;
-
-
-    Qt3DCore::QEntity *rootEntity = window.sceneRootEntity();
-
-    //Create a simple cube
-    Qt3DCore::QEntity *cubeEntity = new Qt3DCore::QEntity(rootEntity);
-    Qt3DExtras::QCuboidMesh *cubeMesh = new Qt3DExtras::QCuboidMesh;
-//    Qt3DExtras::QPhongMaterial *material = new Qt3DExtras::QPhongMaterial;
-    Qt3DCore::QTransform *transform = new Qt3DCore::QTransform;
-
-    WireframeMaterial *material = new WireframeMaterial;
-
-    transform->setTranslation({1,0,0});
-
-    cubeEntity->addComponent(cubeMesh);
+    Qt3DCore::QEntity *cubeEntity = new Qt3DCore::QEntity;
+    cubeEntity->addComponent(new Qt3DExtras::QCuboidMesh);
+    auto material = new Qt3DExtras::QPhongMaterial;
+    material->setAmbient(QColor("red"));
     cubeEntity->addComponent(material);
-    cubeEntity->addComponent(transform);
 
-    Qt3DCore::QEntity *blankEntity = new Qt3DCore::QEntity(cubeEntity);
+    HWindow *window = new HWindow;
+    HWindow *window2 = new HWindow;
 
-    Qt3DCore::QEntity *cubeEntity2 = new Qt3DCore::QEntity(blankEntity);
-    Qt3DExtras::QCuboidMesh *cubeMesh2 = new Qt3DExtras::QCuboidMesh;
-    Qt3DExtras::QPhongMaterial *material2 = new Qt3DExtras::QPhongMaterial;
-    Qt3DCore::QTransform *transform2 = new Qt3DCore::QTransform;
+    window->setScene(cubeEntity);
+    window2->setScene(cubeEntity);
 
-    transform2->setTranslation({0,1,0});
-    material2->setAmbient(QColor(0,0,0));
-    material2->setDiffuse(QColor(0,255,0));
+    auto frameAction1 = window->frameAction();
+    auto frameAction2 = window2->frameAction();
+    QObject::connect(frameAction1, &Qt3DLogic::QFrameAction::triggered, window, &HWindow::shutdown);
+    QObject::connect(frameAction1, &Qt3DLogic::QFrameAction::triggered, window2, &HWindow::turnOn);
+    QObject::connect(frameAction2, &Qt3DLogic::QFrameAction::triggered, window2, &HWindow::shutdown);
+    QObject::connect(frameAction2, &Qt3DLogic::QFrameAction::triggered, window, &HWindow::turnOn);
 
-    cubeEntity2->addComponent(cubeMesh2);
-    cubeEntity2->addComponent(material2);
-    cubeEntity2->addComponent(transform2);
+    window->turnOn();
+//    window2->turnOn();
 
-    //Create a light
-    Qt3DCore::QEntity *lightEntity = new Qt3DCore::QEntity(rootEntity);
-    Qt3DRender::QPointLight *pointLight = new Qt3DRender::QPointLight;
-    Qt3DCore::QTransform *lightTransform = new Qt3DCore::QTransform;
-    lightTransform->setTranslation({0,1.5,5});
-    lightEntity->addComponent(pointLight);
-    lightEntity->addComponent(lightTransform);
-
-    Qt3DRender::QCamera *camera = window.sceneCamera();
-    camera->lens()->setPerspectiveProjection(45.0f, 16.0f/9.0f, 0.1f, 1000.0f);
-
-    Qt3DExtras::QTrackballCameraController *trackball = window.cameraController();
-
-    window.showMaximized();
+    window->show();
+    window2->show();
     return app.exec();
 }
